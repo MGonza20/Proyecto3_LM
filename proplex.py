d72acf7ed2fe534f8bc0dfafbe656085331480ec
Guillermo Santos <guillermo.santosba@gmail.com>
2022-10-05 20:06:03 -0600

    Draw a node

--- proplex.py
+++ proplex.py
@@ -1,12 +1,24 @@
 from ply import lex
 import ply.yacc as yacc
 from uuid import uuid4
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+
+class Incrementer:
+    def __init__(self):
+        self.value = 0
+
+    def increment(self):
+        self.value += 1
 class Node:
-    def __init__(self, value,id=uuid4(),leftChild=None,rightChild=None, parent=None):
+    def __init__(self, value,leftChild=None,rightChild=None, id=None):
         self.value = value
-        self.id = id
         self.leftChild = leftChild
         self.rightChild = rightChild
+        self.id = id
+    def __str__(self):
+        return self.value
 
 tokens = [
     'NOT',
@@ -28,12 +40,14 @@ t_DIMP = r'<\=>'
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_ignore = ' \t'
+labelsDict = {}
 
 # def t_CONST( t ):
 #     r'[0-1]'
 #     t.value = int(t.value)
 #     return t
 
+incr = Incrementer()
 def t_VARIABLE( t ) :
     r'[p|q|r|s|t|u|v|w|x|y|z|1|0]'
     t.value = t.value
@@ -50,45 +64,48 @@ def t_error( t ):
 
 lexer = lex.lex()
 
-
-
-
-
-tree_nodes = []
+G = nx.Graph()
 
 def p_expression_term(p):
     'expression : term'
     p[0] = p[1]
 
 
+
 def p_expression_and(p):
     'expression : expression AND term'
-    p[0] = Node(value="^", leftChild=p[1], rightChild=p[3])
+    p[0] = Node(value="^", leftChild=p[1], rightChild=p[3], id=incr.value)
     # Crear el nodo y dibujar un camino entre su leftChild y rightChild
-    print(p[0].rightChild.value)
+    incr.increment()
 
 def p_expression_or(p):
     'expression : expression OR term'
-    p[0] = Node(value="o", leftChild=p[1], rightChild=p[3])
+    p[0] = Node(value="o", leftChild=p[1], rightChild=p[3], id=incr.value)
     # Crear el nodo y dibujar un camino entre su leftChild y rightChild
-    print(p[0].rightChild.value)
+    G.add_node(incr.value)
+    labelsDict[incr.value] = p[0].value
+    G.add_edge(p[0].id, p[0].leftChild.id)
+    G.add_edge(p[0].id, p[0].rightChild.id)
+    incr.increment()
+
 
 def p_not_expression(p):
     'expression : NOT expression'
     # Crear el nodo y dibujar un camino entre su leftChild
-    p[0] = Node(value="~", leftChild=p[2])
-
+    p[0] = Node(value="~", leftChild=p[2], id=incr.value)
+    incr.increment()
 
 def p_expression_implication(p):
     'expression : expression IMP expression'
-    p[0] = Node(value="=>", leftChild=p[1], rightChild=p[3])
+    p[0] = Node(value="=>", leftChild=p[1], rightChild=p[3], id=incr.value)
     # Crear el nodo y dibujar un camino entre su leftChild y rightChild
+    incr.increment()
 
 def p_expression_dimplication(p):
     'expression : expression DIMP expression'
-    p[0] = Node(value="=>", leftChild=p[1], rightChild=p[3])
+    p[0] = Node(value="=>", leftChild=p[1], rightChild=p[3], id=incr.value)
     # Crear el nodo y dibujar un camino entre su leftChild y rightChild
-
+    incr.increment()
 
 def p_term_factor(p):
     'term : factor'
@@ -96,7 +113,12 @@ def p_term_factor(p):
 
 def p_factor_num(p):
     'factor : VARIABLE'
-    p[0] = Node(value=p[1])
+    p[0] = Node(value=p[1], id=incr.value)
+    G.add_node(incr.value)
+    labelsDict[incr.value] = p[0].value
+    incr.increment()
+
+
     # Crear un nuevo nodo
 #
 # def p_factor_num(p):
@@ -115,7 +137,7 @@ precedence = (
     ( 'left', 'AND', 'OR' )
 )
 
-input = "p^q"
+input = "(qop) o p"
 
 # Give the lexer some input
 lexer.input(input)
@@ -130,3 +152,8 @@ while True:
 print("-- Creando Arbol-- ")
 parser = yacc.yacc()
 result = parser.parse(input)
+
+
+nx.draw_shell(G, nlist=[range(5, 10), range(5)], font_weight='bold', with_labels=True, labels=labelsDict)
+
+plt.show()
